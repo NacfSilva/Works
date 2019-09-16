4dbeca21abb794fe023d1e1955931633c0b225b3
NacfSilva <nacf.silva@gmail.com>
2019-09-16 15:18:05 +0100

    Add files via upload

--- Resources/js/control.js
+++ Resources/js/control.js
@@ -1,8 +1,8 @@
 function myFunction() {
-  if (document.body.scrollTop > (($('#MyWork').offset().top)-90) || document.documentElement.scrollTop >  (($('#MyWork').offset().top))-90) {
+  if (document.body.scrollTop > (($('#MyWork').offset().top)-150) || document.documentElement.scrollTop >  (($('#MyWork').offset().top))-150) {
        if (document.body.scrollTop >  (($('#About').offset().top)-50) || document.documentElement.scrollTop > (($('#About').offset().top)-50)  ) {
              if (document.body.scrollTop >  (($('#Contact').offset().top)-50) || document.documentElement.scrollTop > (($('#Contact').offset().top)-50)  ) {
-    document.getElementById("myMenu").className = "navbar sticky-top navbar-expand-lg navbar-dark border-bottom w-100 borde-ochre bg-pastel";
+    document.getElementById("myMenu").className = "navbar sticky-top navbar-expand-lg navbar-light border-bottom w-100 borde-ochre bg-pastel";
     document.getElementById("myLogo").className = "changeLogo logo1 socail-vertical mt-2";
     document.getElementById("Silva").className = "Silva3";
       document.getElementById("social-em").className = "fa fa-envelope fa-2x social black-text social-vertical ";
@@ -45,7 +45,7 @@ function myFunction() {
       document.getElementById("menu-size3").className = "menu-size white-text mb-3";
     }
   }else{
-      document.getElementById("myMenu").className = "navbar sticky-top navbar-expand-lg navbar-dark border-bottom w-100 borde-redrush bg-white";
+      document.getElementById("myMenu").className = "navbar sticky-top navbar-expand-lg navbar-light border-bottom w-100 borde-redrush bg-white";
     document.getElementById("myLogo").className = "changeLogo logo1 socail-vertical mt-2";
     document.getElementById("Silva").className = "Silva2";
       document.getElementById("social-em").className = "fa fa-envelope fa-2x social black-text social-vertical ";
